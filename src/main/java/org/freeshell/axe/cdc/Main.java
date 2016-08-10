54dd1ce9610949e8f9c550d5d46f01806feb9345
Axel Knauf <axel.knauf@posteo.de>
2016-08-10 07:56:48 +0200

    Cleanup, comments and whitespace

--- src/main/java/org/freeshell/axe/cdc/Main.java
+++ src/main/java/org/freeshell/axe/cdc/Main.java
@@ -8,62 +8,58 @@ import javax.swing.*;
 public class Main {
 
   public static void main(String[] argv) {
+    if (!SystemTray.isSupported()) {
+      System.err.println("SystemTray is not supported, exiting.");
+      System.exit(1);
+    }
     SwingUtilities.invokeLater(new Runnable() {
-        public void run() {
-            createAndShowGUI();
-        }
+      public void run() {
+        createAndShowGUI();
+      }
     });
   }
 
-
   private static void createAndShowGUI() {
-      //Check the SystemTray support
-      if (!SystemTray.isSupported()) {
-          System.out.println("SystemTray is not supported");
-          return;
+    final PopupMenu popup = new PopupMenu();
+    // FIXME: replace with own icon
+    final TrayIcon trayIcon =
+      new TrayIcon(createImage("/images/bulb.gif", "tray icon"));
+    final SystemTray tray = SystemTray.getSystemTray();
+
+    MenuItem exitItem = new MenuItem("Exit");
+    popup.add(exitItem);
+    trayIcon.setPopupMenu(popup);
+
+    try {
+      tray.add(trayIcon);
+    } catch (AWTException e) {
+      System.err.println("TrayIcon could not be added.");
+      return;
+    }
+    trayIcon.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null,
+            "This dialog box is run from System Tray");
       }
-      final PopupMenu popup = new PopupMenu();
-      final TrayIcon trayIcon =
-             new TrayIcon(createImage("/images/bulb.gif", "tray icon"));
-      final SystemTray tray = SystemTray.getSystemTray();
-      
-      // Create a popup menu components
-      MenuItem exitItem = new MenuItem("Exit");
-      popup.add(exitItem);
-       trayIcon.setPopupMenu(popup);
-      
-       try {
-           tray.add(trayIcon);
-       } catch (AWTException e) {
-           System.out.println("TrayIcon could not be added.");
-           return;
-       }
-       trayIcon.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent e) {
-               JOptionPane.showMessageDialog(null,
-                       "This dialog box is run from System Tray");
-           }
-       });
-      
-      
-      exitItem.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-              tray.remove(trayIcon);
-              System.exit(0);
-          }
-      });
+    });
+
+    exitItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        tray.remove(trayIcon);
+        System.exit(0);
+      }
+    });
   }
-  
-   //Obtain the image URL
-   protected static Image createImage(String path, String description) {
-       URL imageURL = Main.class.getResource(path);
-       
-       if (imageURL == null) {
-           System.err.println("Resource not found: " + path);
-           return null;
-       } else {
-           return (new ImageIcon(imageURL, description)).getImage();
-       }
-   }
+
+  protected static Image createImage(String path, String description) {
+    URL imageURL = Main.class.getResource(path);
+
+    if (imageURL == null) {
+      System.err.println("Resource not found: " + path);
+      return null;
+    } else {
+      return (new ImageIcon(imageURL, description)).getImage();
+    }
+  }
 
 }
